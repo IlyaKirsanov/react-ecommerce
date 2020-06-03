f809a094c74cd904e77347d65815cef7abfc7ef0
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-06-03 15:44:04 +0300

    uppdating with types

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
@@ -45,13 +45,17 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SingInAndSignUpPage} />
+          <Route path="/signin" render={()=> this.props.currentUser ? (<Redirect to='/'/>) : (<SingInAndSignUpPage/>)} />
         </Switch>
       </div>
     );
   }
 
 }
+
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch =>({
   setCurrentUser: user=>dispatch(setCurrentUser(user))
