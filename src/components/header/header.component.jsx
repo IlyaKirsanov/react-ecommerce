09d716ac16e7738e79254f8a2451f028f47cde75
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-06-03 16:49:46 +0300

    redux in cart comp

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import {connect} from 'react-redux';
 
-const Header = ({ currentUser }) =>
+const Header = ({ currentUser, hidden}) =>
 	(
 		<div className='header'>
 			<Link className='logo-container' to='/'>
@@ -29,13 +29,14 @@ const Header = ({ currentUser }) =>
 				}
 				<CartIcon/>
 			</div>
-			<CartDropdown/>
+			{ hidden ? null : <CartDropdown />}
 		</div>
 
 	)
 
-const mapStateToProps = (state) =>({
-	currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) =>({
+	currentUser,
+	hidden
 })
 
 export default connect(mapStateToProps)(Header);
