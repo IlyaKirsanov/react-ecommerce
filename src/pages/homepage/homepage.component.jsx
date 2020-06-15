0ca5f8ce4f2713d68cf061d6cae1aae99ee87428
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-06-15 16:38:59 +0300

    styled components added part1

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Directory from "../../components/directory/directory.component";
-import './homepage.styles.scss'
+import {HomePagecontainer} from './homepage.styles'
+
 
 const HomePage = () => (
-    <div className='homepage'>
-       <Directory/>
-    </div>
+  <HomePagecontainer>
+    <Directory/>
+  </HomePagecontainer>
 
     )
 
